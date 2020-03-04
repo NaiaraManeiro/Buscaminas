cacb0028d9827c4751cf254ef8973f2754d3aebd
Aitor <aitorjavierperez605@gmail.com>
2020-03-04 14:47:28 +0100

    Cambios en la clase Tablero

--- src/java/packModelo/Tablero.java
+++ src/java/packModelo/Tablero.java
@@ -3,19 +3,37 @@ package packModelo;
 public class Tablero {
 
     private static Tablero mTablero;
-    private Casilla[][] matriz;
+    private packModelo.Casilla[][] matriz;
     private int altura;
     private int anchura;
     private int minas;
 
     private Tablero(){}
 
-    public Tablero generarTablero(){
-        Tablero tablero = null; //Lo he puesto así para que no salga error
-
-        return tablero;
+    public void generarTablero(){
+        matriz = new Casilla[altura-1][anchura-1];
+        for (int i = 0; i<altura;i++){
+            for(int j = 0; j<anchura;j++){
+                matriz[i][j] = new CasillaVacia(false,false);
+            }
+        }
     }
+    public static Tablero getmTablero(){
+        if(mTablero == null) mTablero = new Tablero();
+        return mTablero;
+    }
+    public Casilla devolverCasilla(int x, int y) {
+        return matriz[x][y];
+    }
+    public void marcarDesmarcarCasilla(int x, int y){
+        matriz[x][y].marcarCasilla();
+    }
+    private void ponerMinas(){
 
+    }
+    private void ponerNumeros(){
+
+    }
     public void setAltura(int pAltura){
         this.altura = pAltura;
     }
@@ -24,20 +42,7 @@ public class Tablero {
         this.anchura = pAnchura;
     }
 
-    public void setMinas(int pMinas){
-        this.minas = pMinas;
-    }
+    public void setMinas(int pMinas){ this.minas = pMinas; }
 
-    public static Tablero getmTablero(){
-        return mTablero;
-    }
-
-    public int getFilas() {
-        return altura;
-    }
-
-    public int getColumnas(){
-        return anchura;
-    }
-
+    public int getFilas(){ return altura; }
 }
