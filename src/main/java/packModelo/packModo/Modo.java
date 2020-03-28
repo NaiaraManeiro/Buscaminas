9cd6829d1beb2e2a0cb7ae2f50a47ddbf95f5603
Naiara <naiaramaneiro07@gmail.com>
2020-03-28 11:47:15 +0100

    Error arreglado en Modo

--- src/main/java/packModelo/packModo/Modo.java
+++ src/main/java/packModelo/packModo/Modo.java
@@ -12,6 +12,7 @@ public class Modo {
     public Modo(int pNivel){
         this.numero = pNivel;
     }
+
     public Tablero generarTablero() {
         this.tablero = new Tablero();
         asignarParametros();
@@ -33,6 +34,7 @@ public class Modo {
         ponerNumeros();
         return tablero;
     }
+
     private void asignarParametros(){
         if (numero == 1){
             tablero.setAltura(7);
@@ -42,12 +44,13 @@ public class Modo {
             tablero.setAltura(10);
             tablero.setAnchura(15);
             tablero.setMinas(30);
-        } else if (numero == 1){
+        } else if (numero == 3){
             tablero.setAltura(12);
             tablero.setAnchura(25);
             tablero.setMinas(75);
-        }else{}
+        }
     }
+
     private void ponerMinas(){
         boolean hayMina;
         int fila, columna;
@@ -63,6 +66,7 @@ public class Modo {
             tablero.setCasilla(new CasillaMina(new NoClicada(new Coordenada(columna, fila)), new Coordenada(columna, fila)));
         }
     }
+
     private void ponerNumeros(){
         for (int i = 0; i < tablero.getFilas(); i++) {
             for (int j = 0; j < tablero.getColumnas(); j++) {
@@ -72,6 +76,7 @@ public class Modo {
             }
         }
     }
+
     private void incrementarCasilla(int pColumna, int pFila){
         // si las coordenadas están dentro del tablero, entonces
         if ((pFila >= 0 && pColumna >= 0)&&(pFila < tablero.getFilas() && pColumna < tablero.getColumnas())){
@@ -79,6 +84,7 @@ public class Modo {
             if (tablero.devolverCasilla(pColumna, pFila) instanceof CasillaNormal) ((CasillaNormal) tablero.devolverCasilla(pColumna, pFila)).incrementarNumero();
         }
     }
+
     // incrementamos o ponemos numero a la coordenada seleccionada (los parámetros son las coordenadas donde se encuentra la mina)
     private void incrementarAdyacentes(int x, int y){
         /*
